353af5fce0abf32f95cc179bc02e42c3e307314e
Adarsh Goswami <adarsh010120@gmail.com>
2020-08-06 20:55:11 +0530

    BookActivity

--- app/src/main/java/com/example/mylibrary/BooksRecViewAdapter.java
+++ app/src/main/java/com/example/mylibrary/BooksRecViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mylibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,7 +53,8 @@ public class BooksRecViewAdapter extends RecyclerView.Adapter<BooksRecViewAdapte
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, books.get(position).getName()+"Selected", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(mContext, BookActivity.class);
+                mContext.startActivity(intent);
             }
         });
 

e50f7bff438c7957396a31237982c2b17f446b5e
Adarsh Goswami <adarsh010120@gmail.com>
2020-08-10 20:00:13 +0530

    Scroll View

--- app/src/main/java/com/example/mylibrary/BookActivity.java
+++ app/src/main/java/com/example/mylibrary/BookActivity.java
@@ -30,9 +30,7 @@ public class BookActivity extends AppCompatActivity {
         read= findViewById(R.id.bookAlreadyRead);
         page= findViewById(R.id.bookPage);
 
-        Books book=new Books("Death Note", "Adarsh Goswami", "Write the name of the person and he will die", "Overview of inheritance, Working with subclasses and superclasses, " +
-                "Overriding methods in the superclass, Introducing polymorphism, Creating and extending abstract" +
-                "classes, Modeling business problems using Java classes, Making classes immutable", "https://i.pinimg.com/474x/59/94/15/5994155f280a34fcd94b0ee0a7507448.jpg", 100, 124);
+        Books book=new Books("Death Note", "Adarsh Goswami", "Write the name of the person and he will die", "\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"", "https://i.pinimg.com/474x/59/94/15/5994155f280a34fcd94b0ee0a7507448.jpg", 100, 124);
         setData(book);
     }
 
